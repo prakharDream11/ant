408399471d36921d5404b4038b4c4408921187bb
Prakhar Dixit <prakhar.dixit@dream11.com>
2022-01-16 21:21:44 +0530

    performance

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button , Pagination, Input , Progress , Alert } from 'antd';
+import 'antd/dist/antd.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+       <Button type="primary">Primary Button</Button>
+       <Pagination defaultCurrent={1} total={50} />
+       <Input placeholder="Basic usage" />
+       <Alert message="Success Text" type="success" />
+       <Progress type="circle" percent={75} />
+   </>
   );
 }
 
